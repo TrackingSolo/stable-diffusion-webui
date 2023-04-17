2255b9997bb05e7d361b95d95a4e77f2d94368d7
TrackingSolo <129760085+TrackingSolo@users.noreply.github.com>
2023-04-17 06:58:06 -0600

    As of 17APR

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,9 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\Users\jason\AppData\Local\Programs\Python\Launcher\py.exe
 set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=
+set VENV_DIR=C:\Users\jason\AI\Flavors\Auto1111\stable-diffusion-webui\venv
+set COMMANDLINE_ARGS=--skip-torch-cuda-test --precision full --no-half --medvram
 
+git pull
 call webui.bat
